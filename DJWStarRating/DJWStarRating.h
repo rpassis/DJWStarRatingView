f0fa0f39819d8c1f572894a8d92a2bed7bab71d2
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-13 19:45:27 +1000

    Added as framework

---
+++ DJWStarRating/DJWStarRating.h
@@ -0,0 +1,20 @@
+//
+//  DJWStarRating.h
+//  DJWStarRating
+//
+//  Created by Rogerio de Paula Assis on 13/09/2016.
+//  Copyright © 2016 Daniel Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DJWStarRating.
+FOUNDATION_EXPORT double DJWStarRatingVersionNumber;
+
+//! Project version string for DJWStarRating.
+FOUNDATION_EXPORT const unsigned char DJWStarRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJWStarRating/PublicHeader.h>
+
+#import <DJWStarRating/DJWStarRatingView.h>
+
